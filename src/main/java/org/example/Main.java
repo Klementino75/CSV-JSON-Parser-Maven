2f9a1608720a24dba5a089b6542a6c614dd3f74a
Klementino75 <klementino75@ya.ru>
2024-08-26 01:04:27 +0300

    Added 3 tasks.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,13 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
@@ -17,29 +21,12 @@ public class Main {
 
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
+        String fileName2 = "data.json";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
-    }
-
-    private static void writeString(String json) {
-        try (FileWriter file = new FileWriter("data.json")) {
-            file.write(json);
-            file.flush();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static String listToJson(List<Employee> list) {
-        Gson gson = new GsonBuilder()
-                .setPrettyPrinting()
-                .create();
-        String json = gson.toJson(list);
-//        Type listType = new TypeToken<List<Employee>>(){}.getType();
-//        String json = gson.toJson(list, listType);
-        System.out.println(json);
-        return json;
+        json = String.valueOf(readToString(fileName2));
+        jsonToList(json);
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -59,5 +46,49 @@ public class Main {
         }
         assert csv != null;
         return staff;
+    }
+
+    private static String listToJson(List<Employee> list) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        String json = gson.toJson(list);
+//        или так
+//        Type listType = new TypeToken<List<Employee>>(){}.getType();
+//        String json = gson.toJson(list, listType);
+        System.out.println(json);
+        return json;
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static JSONArray readToString(String fileName2) {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject;
+        JSONArray array = null;
+        try {
+            array = (JSONArray) parser.parse(new FileReader(fileName2));
+            for (Object item : array) {
+                jsonObject = (JSONObject) item;
+                System.out.println(jsonObject);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return array;
+    }
+
+    private static void jsonToList(String json) {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        List<Employee> list = gson.fromJson(json, listType);
+        list.forEach(System.out::println);
     }
 }
